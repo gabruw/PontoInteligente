cb1c6d8adfff76cf331fd673c7925f3c15e20024
Snyp <gabrielmarques.mg@gmail.com>
2019-12-25 12:06:02 -0300

    [Fix]_TryOne

--- src/main/java/com/kazumi/pontointeligente/api/entities/Lancamento.java
+++ src/main/java/com/kazumi/pontointeligente/api/entities/Lancamento.java
@@ -1,13 +1,8 @@
 package com.kazumi.pontointeligente.api.entities;
 
-import java.beans.Transient;
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -17,7 +12,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
