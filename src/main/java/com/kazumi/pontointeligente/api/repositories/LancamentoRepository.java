9d1d12805febb97906237c2d4c0a856eb4841294
Snyp <gabrielmarques.mg@gmail.com>
2019-12-26 08:17:44 -0300

    [Add][Fix]_Reps and Fix in Test's

---
+++ src/main/java/com/kazumi/pontointeligente/api/repositories/LancamentoRepository.java
@@ -0,0 +1,24 @@
+package com.kazumi.pontointeligente.api.repositories;
+
+import java.util.List;
+
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import com.kazumi.pontointeligente.api.entities.Lancamento;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@NamedQueries({
+        @NamedQuery(name = "LancamentoRepository.findByFuncionarioId", query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+    List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+
+    Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
+}
