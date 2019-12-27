3b844a2e422d35765b77c7b6e149e0dfdecffd1e
Snyp <gabrielmarques.mg@gmail.com>
2019-12-27 07:58:51 -0300

    Update LancamentoRepositoryTest.java

--- src/test/java/com/kazumi/pontointeligente/api/repositories/LancamentoRepositoryTest.java
+++ src/test/java/com/kazumi/pontointeligente/api/repositories/LancamentoRepositoryTest.java
@@ -11,7 +11,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
